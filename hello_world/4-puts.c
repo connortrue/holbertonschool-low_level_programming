5df8f1efad749ac1da422d4097b07c0d6072cb04
Connor True <connor.true.94@gmail.com>
2023-05-24 17:12:47 -0700

    less goooooo

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This prints text to the screen.
+ *
+ * Return: Returns 0 when it works.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
