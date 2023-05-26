1f1c0812a1f1c0732280cf6d0024d08c3a368773
Connor True <connor.true.94@gmail.com>
2023-05-26 15:55:03 -0700

    same as chars

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints every character in the alphabet, lowercase, ending with a nl
+ *
+ * Return: returns 0 when it works
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
