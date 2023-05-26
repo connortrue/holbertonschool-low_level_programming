0e78df95e9dcae4cdedf81633e54b1f8439e5e5d
Connor True <connor.true.94@gmail.com>
2023-05-26 15:03:27 -0700

    fixed me comment

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints every character in the alphabet, lowercase, ending with a nl
+ *
+ * Return: returns 0 when it works
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
