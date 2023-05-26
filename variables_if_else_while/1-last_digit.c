c82dc4cc9c9dab97b5fc4f665ae3935f83017477
Connor True <connor.true.94@gmail.com>
2023-05-26 13:45:43 -0700

    passes betty and gcc

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This makes a random number and tests to see if it's positve, negeativ
+ * e, or zero, and prints the result
+ *
+ * Return: Returns zero if it works properly.
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n == 0)
+		printf("and is 0\n");
+	else if (n > 5)
+		printf("and is greater than 5\n");
+	else if (n < 6)
+		printf("and is less than 6\n");
+	return (0);
+}
