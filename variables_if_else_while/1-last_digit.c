a19fb9461f4ffc3c4df3bc1377cabcb56945f03e
Connor True <connor.true.94@gmail.com>
2023-05-26 14:02:51 -0700

    one last word change

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	if (n % 10 > 5 && n % 10 != 0)
 		printf("and is greater than 5\n");
 	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6\n");
+		printf("and is less than 6 and not 0\n");
 	else
 		printf("and is 0\n");
 	return (0);
