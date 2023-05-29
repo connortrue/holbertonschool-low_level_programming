7a3dbee142b04ff3d7932cc194fb762bb0661357
Connor True <connor.true.94@gmail.com>
2023-05-29 11:33:17 -0700

    betty only needed two things that time

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 /**
- * _islower - takes the int c, the ASCII value of a character and checks if it'
- * s in the range of the lowercase alphabet.
+ * _isalpha - takes the int c, the ASCII value of a character and checks if it'
+ * s in the range of the alphabet, upper or lowercase.
  *
- * Return: 1 if it is and 0 if it ain't (a lowercase letter)
+ * Return: 1 if it is and 0 if it ain't (a letter)
  *
  * @c: an int holding the ascii value of a character
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
