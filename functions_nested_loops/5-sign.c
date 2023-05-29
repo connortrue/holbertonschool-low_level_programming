0141413a131cc5c8f96affbf0be62952897b2d16
Connor True <connor.true.94@gmail.com>
2023-05-29 12:21:05 -0700

    yeye comments done

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <unistd.h>
-
+/**
+ * print_sign - tests whether the number is positive or negative and prints
+ * the sign (+, -, or 0)
+ * Return: returns 1 if it's positive, -1 if it's negative, and 0 if 0
+ * @n: and integer holding a number to test
+ */
 int print_sign(int n)
 {
 	if (n > 0)
