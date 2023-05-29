b11762432867be6bf5b5a14d5678ac3092cf9e98
Connor True <connor.true.94@gmail.com>
2023-05-29 12:48:14 -0700

    celared betty now to eart

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,16 @@
 #include "main.h"
+/**
+ * _abs - takes a number and retunrs it's absolute value
+ *
+ * @n: the number to be tested
+ *
+ * Return: returns the absolute value of the number passed
+ */
 
 int _abs(int n)
 {
 	if (n < 0)
-		return -n;
+		return (-n);
 	else
-		return n;
+		return (n);
 }
