5d709c245d81a8ae97808ef3bff40a333d19a09e
Connor True <connor.true.94@gmail.com>
2023-05-29 14:15:42 -0700

    finally betty likes it

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ * print_last_digit - takes a number and returns the abs val of the last digit
+ *
+ * @n: the number we're recieving
+ * Return: returns the abs value of the last digit
+ */
 
 int print_last_digit(int n)
 {
 	int lastn = n % 10;
+
 	if (lastn < 0)
 		lastn = -lastn;
 	_putchar(lastn + '0');
