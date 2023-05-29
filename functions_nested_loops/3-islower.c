8c797dc9668e2e9b6626202ddbcaef675662220e
Connor True <connor.true.94@gmail.com>
2023-05-29 10:29:08 -0700

    cleared the betty issues with style

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 /**
- * _islower - takes the int c, the ASCII value of a character and checks if it's
- * in the range of the lowercase alphabet.
+ * _islower - takes the int c, the ASCII value of a character and checks if it'
+ * s in the range of the lowercase alphabet.
  *
- * Return: 1 if it is
+ * Return: 1 if it is and 0 if it ain't (a lowercase letter)
  *
- * Return: 0 if it ain't
+ * @c: an int holding the ascii value of a character
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
