3d25ecf31264267f5405467bde381af05f72f242
Connor True <connor.true.94@gmail.com>
2023-05-30 15:17:43 -0700

    its a while and an if

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,11 @@ void print_to_98(int n)
 	while (n < 99)
 	{
 		printf("%d", n);
-		putchar(',');
-		putchar(' ');
+		if (n < 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
 	putchar('\n');
