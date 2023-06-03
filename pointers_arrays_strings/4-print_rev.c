9b7e160ff035b80308300da429bbbeb61c9d82d0
Connor True <connor.true.94@gmail.com>
2023-06-03 09:19:48 -0700

    thar she blows

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+/**
+ * print_rev - Prints a string in reverse order followed by a new line.
+ *
+ * @s: The string to be printed in reverse order.
+ *
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	/* Find the length of the string */
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Print the string in reverse order */
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	/* Print a new line character */
+	_putchar('\n');
+}
