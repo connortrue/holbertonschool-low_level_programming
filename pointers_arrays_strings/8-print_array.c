eb83746ca015b12a77105033db73496afde60704
Connor True <connor.true.94@gmail.com>
2023-06-03 12:50:08 -0700

    betcha this'll do

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,6 +14,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	/* Check if the length is over 0 */
+	if (n <= 0)
+	{
+		_putchar('\n');
+		_putchar('\n');
+		return;
+	}
+
 	/* Print the first element */
 	printf("%d", a[0]);
 
