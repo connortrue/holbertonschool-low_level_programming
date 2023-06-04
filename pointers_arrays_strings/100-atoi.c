98ca100c70728cf488c2f877f6ddfb6fd67de8ab
Connor True <connor.true.94@gmail.com>
2023-06-03 18:38:24 -0700

    try this

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,32 @@
  *
  * Return: integer value of string
  */
-
 int _atoi(char *s)
 {
-	int sign = 1;
-	int res = 0;
-	int i = 0;
+    int sign = 1;
+    int res = 0;
+    int i = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
+    while (s[i] == ' ')
+        i++;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			res = res * 10 + s[i] - '0';
+    if (s[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (s[i] == '+')
+        i++;
 
-		i++;
-	}
+    while (s[i] != '\0')
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+            res = res * 10 + s[i] - '0';
+        else
+            break;
 
-	return (res * sign);
+        i++;
+    }
+
+    return (res * sign);
 }
