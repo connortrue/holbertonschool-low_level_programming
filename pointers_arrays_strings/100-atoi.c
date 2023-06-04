efd7d40ebd0a1cb2b1f0e0ff73fa2823fe03fa19
Connor True <connor.true.94@gmail.com>
2023-06-03 21:01:54 -0700

    more of em

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -29,7 +29,7 @@ int _atoi(char *s)
     {
         if (s[i] >= '0' && s[i] <= '9')
         {
-            if ((res > INT_MAX / 10) || ((res == INT_MAX / 10) && ((s[i] - '0') > INT_MAX % 10)))
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
             {
                 if (sign == 1)
                     return (INT_MAX);
