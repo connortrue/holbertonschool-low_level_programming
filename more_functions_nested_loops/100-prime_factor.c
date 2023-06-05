c4ecf460ad06c99fa571a4b1e9ae9b35c6f32d60
Connor True <connor.true.94@gmail.com>
2023-06-05 14:22:27 -0700

    buddyyyyyy

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 when done
+ */
+
+/* Function to find the maximum prime factor of a given number */
+int main(void)
+{
+	double n = 612852475143;
+
+	printf("%d\n", maxPrimeFactor(n));
+	return (0);
+}
+
+/**
+ * maxPrimeFactor - finds the highest prime factor of a passed double
+ * @n: number being tested
+ *
+ * Return: returns the highest prime number found
+ */
+
+int maxPrimeFactor(double n)
+{
+	int maxPrime = -1;
+	int i;
+
+	/* Divide n by 2 until it is odd */
+	while (fmod(n, 2) == 0)
+	{
+		maxPrime = 2;
+		n /= 2;
+	}
+
+	/* Divide n by odd numbers starting from 3 */
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (fmod(n, i) == 0)
+		{
+			maxPrime = i;
+			n /= i;
+		}
+	}
+
+	/* If n is still greater than 2, it is a prime number */
+	if (n > 2)
+		maxPrime = n;
+
+	return (maxPrime);
+}
