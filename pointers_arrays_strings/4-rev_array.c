c58742e74cd9bca46e66e9b1a11cc5116ac32ea2
Connor True <connor.true.94@gmail.com>
2023-06-08 15:53:00 -0700

    lets try this

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * print_rev - Prints a string in reverse order followed by a new line.
+ *   *
+ *    * @s: The string to be printed in reverse order.
+ *     *
+ *      * Return: void.
+ *       */
+void reverse_array(int *a, int n)
+{
+	/* Print the string in reverse order */
+	for (--n; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+
+	/* Print a new line character */
+	_putchar('\n');
+}
