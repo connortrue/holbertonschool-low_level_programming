c6dc9153ddf43a31c5e0c6ecc1a5cf79b3c37cfc
Connor True <connor.true.94@gmail.com>
2023-06-08 17:36:34 -0700

    testing this one

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+	return s;
+}
