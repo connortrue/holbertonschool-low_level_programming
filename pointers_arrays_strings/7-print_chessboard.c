33e04b6beee6bd015c6f425d5d7b83f69b66f2cf
Connor True <connor.true.94@gmail.com>
2023-06-12 15:52:38 -0700

    now try it

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -15,7 +15,7 @@ void print_chessboard(char (*a)[8])
 	{
 		/* Loop through columns */
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]); /* Print character */
-		putchar('\n'); /* New line after each row */
+			_putchar(a[i][j]); /* Print character */
+		_putchar('\n'); /* New line after each row */
 	}
 }
