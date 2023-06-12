5761c3660958ba64b8846bfb400cd02295b86f55
Connor True <connor.true.94@gmail.com>
2023-06-12 15:51:34 -0700

    simple chess board

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * print_chessboard - prints the chessboard
+ * @a: 2D array representing the chessboard
+ *
+ * Description: This function prints the chessboard represented by a 2D
+ * array of characters.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	/* Loop through rows */
+	for (i = 0; i < 8; i++)
+	{
+		/* Loop through columns */
+		for (j = 0; j < 8; j++)
+			putchar(a[i][j]); /* Print character */
+		putchar('\n'); /* New line after each row */
+	}
+}
