17304dd5661a91bf32a7308f94c744b186a2edcf
Connor True <connor.true.94@gmail.com>
2023-06-16 14:46:39 -0700

    well that was easy

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,14 @@
-void main(void)
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of command line arguments
+ * @argv: an array of pointers to the command line arguments
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
 {
-	puts(argv[0]);
+	printf("%s\n", argv[0]);
+	return (0);
 }
