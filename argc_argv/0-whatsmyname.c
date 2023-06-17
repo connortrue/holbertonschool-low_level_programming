8aa2e8342874990fac599816a5ce0ca2ea30c257
Connor True <connor.true.94@gmail.com>
2023-06-16 17:19:06 -0700

    added some things

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the name of the program
@@ -7,8 +8,9 @@
  *
  * Return: 0 if successful
  */
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
