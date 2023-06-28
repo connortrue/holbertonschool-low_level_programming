729143ab8b40d8f0c68c46f1abfbd5e803c031bf
Connor True <connor.true.94@gmail.com>
2023-06-28 13:59:08 -0700

    let's see if you have the _ versions of things or if i have to put them in myself

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct for a dog
+ * @name: the dog's name, a string
+ * @age: dog's age, a float
+ * @owner: the name of the dogs owner, a string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = malloc(_strlen(name) + 1);
+	_strcpy(d->name, name);
+	d->age = age;
+	d->owner = malloc(_strlen(owner) + 1);
+	_strcpy(d->owner, owner);
+}
