ea061149a0ffe48367d9c97c77bfeb52ae82b33c
Connor True <connor.true.94@gmail.com>
2023-06-28 12:56:46 -0700

    try this

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a struct for a dog
+ * @name: the dog's name, a string
+ * @age: dog's age, a float
+ * @owner: the name of the dogs owner, a string
+ */
+
 struct dog
 {
 	char *name;
