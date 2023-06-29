2838bb982a95027f90ff9efa252e8d2ef7fa05d6
Connor True <connor.true.94@gmail.com>
2023-06-29 08:49:25 -0700

    just assign em

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -17,9 +17,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = _malloc(_strlen(name) + 1);
-	_strcpy(d->name, name);
+	d->name = name;
 	d->age = age;
-	d->owner = _malloc(_strlen(owner) + 1);
-	_strcpy(d->owner, owner);
+	d->owner = owner;
 }
