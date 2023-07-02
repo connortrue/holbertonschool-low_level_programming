da9b4bb032efc8c9b92f6b291c0f9104a756a5e1
Connor True <connor.true.94@gmail.com>
2023-07-02 11:15:26 -0700

    adadded a null check

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,6 +8,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	/* first check if null */
+	if (d == NULL)
+		return;
+	/* assign the passed vars */
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
