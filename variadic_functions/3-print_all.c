647a4f8c0f5947124c1ae4dbca2da0893cb84bf9
Connor True <connor.true.94@gmail.com>
2023-07-03 11:38:33 -0700

    lets try this one

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,6 +11,7 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0;
 	char *str;
+	char *separator = "";
 
 	/* initialize args variable */
 	va_start(args, format);
@@ -21,27 +22,25 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int));
+				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (str != NULL)
-					printf("%s", str);
-				else
-					printf("(nil)");
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
 				break;
 			default:
 				i++;
 				continue;
 		}
-		if (format[i + 1] != '\0')
-			printf(", ");
+		separator = ", ";
 		i++;
 	}
 	/* clean up args variable */
