4eaa8ef6dd9d385271304cbffd31f32b8f8d86e8
connortrue <connor.true.94@gmail.com>
2023-07-14 11:44:48 -0700

    i wouldnt doubt ti

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,8 +12,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0; /* initialize result to zero */
 	int i = 0; /* initialize index to zero */
 
+	if (b == NULL) /* check if b is NULL */
+	{
+		return (0); /* return 0 if b is NULL */
+	}
+
 	while (b[i] != '\0') /* loop until the end of the string */
 	{
+		if (b[i] != '0' && b[i] != '1') /* check if the curchar is not 0 or 1 */
+		{
+			return (0); /* return 0 if the input string isn't in binary */
+		}
 		result = result * 2; /* shift result to the left by one bit */
 		if (b[i] == '1') /* if the current bit is one */
 		{
