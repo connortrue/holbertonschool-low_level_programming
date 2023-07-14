122e349c760dc389672b46683c9fc056fb0aae5c
connortrue <connor.true.94@gmail.com>
2023-07-14 12:55:47 -0700

    bribes = candy

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,9 +8,9 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1) /* recursively call print_binary with n divided by 2 */
-    {
-        print_binary(n >> 1);
-    }
-    _putchar((n & 1) + '0'); /* print the last digit of n */
+	if (n > 1) /* recursively call print_binary with n divided by 2 */
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0'); /* print the last digit of n */
 }
