97f73b6c23bad4e2f60d0b32eb1d13fdd31bfc3f
connortrue <connor.true.94@gmail.com>
2023-07-16 13:10:26 -0500

    betty hopefully, its not on my machine

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,24 +1,30 @@
 #include "main.h"
-
+/**
+ * create_file - creates a file and writes text_content to it
+ * @filename: name of the file to create
+ * @text_content: content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int fd; /* file descriptor */
-    ssize_t len = 0; /* length of content written */
+	int fd; /* file descriptor */
+	ssize_t len = 0; /* length of content written */
 
-    if (!filename) /* check if filename is NULL */
-        return (-1);
+	if (!filename) /* check if filename is NULL */
+		return (-1);
 
-    /* open file with specified filename and flags */
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1) /* check if open failed */
-        return (-1);
+	/* open file with specified filename and flags */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1) /* check if open failed */
+		return (-1);
 
-    if (text_content) /* check if text_content is not NULL */
-        /* write content to file */
-        len = write(fd, text_content, strlen(text_content));
+	if (text_content) /* check if text_content is not NULL */
+		/* write content to file */
+		len = write(fd, text_content, strlen(text_content));
 
-    close(fd); /* close file */
+	close(fd); /* close file */
 
-    /* return -1 if write failed, 1 otherwise */
-    return (len == -1 ? -1 : 1);
+	/* return -1 if write failed, 1 otherwise */
+	return (len == -1 ? -1 : 1);
 }
--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,24 +1,30 @@
 #include "main.h"
-
+/**
+ * append_text_to_file - appends text_content to a file
+ * @filename: name of the file to append to
+ * @text_content: content to append to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd; /* file descriptor */
-    ssize_t len = 0; /* length of content written */
+	int fd; /* file descriptor */
+	ssize_t len = 0; /* length of content written */
 
-    if (!filename) /* check if filename is NULL */
-        return (-1);
+	if (!filename) /* check if filename is NULL */
+		return (-1);
 
-    /* open file with specified filename and flags */
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1) /* check if open failed */
-        return (-1);
+	/* open file with specified filename and flags */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1) /* check if open failed */
+		return (-1);
 
-    if (text_content) /* check if text_content is not NULL */
-        /* append content to file */
-        len = write(fd, text_content, strlen(text_content));
+	if (text_content) /* check if text_content is not NULL */
+		/* append content to file */
+		len = write(fd, text_content, strlen(text_content));
 
-    close(fd); /* close file */
+	close(fd); /* close file */
 
-    /* return -1 if write failed, 1 otherwise */
-    return (len == -1 ? -1 : 1);
+	/* return -1 if write failed, 1 otherwise */
+	return (len == -1 ? -1 : 1);
 }
