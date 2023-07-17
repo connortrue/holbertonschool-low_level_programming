e1bd7e4770dfc022a29795cd418b2f233c066230
connortrue <connor.true.94@gmail.com>
2023-07-17 16:10:09 -0500

    betty

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -9,48 +9,38 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp, *prev; /* pointers to the current and previous nodes */
-    unsigned int i; /* loop counter */
+	listint_t *temp, *prev; /* pointers to the current and previous nodes */
+	unsigned int i; /* loop counter */
 
-    /* check if the linked list is empty */
-    if (*head == NULL)
-        return (-1);
-
-    /* initialize the pointers */
-    temp = *head;
-    prev = NULL;
-
-    /* check if the index is 0 */
-    if (index == 0)
-    {
-        /* update the head pointer */
-        *head = temp->next;
-
-        /* free the current node */
-        free(temp);
-
-        return (1);
-    }
-
-    /* iterate through the linked list to find the node at index `index` */
-    for (i = 0; temp != NULL && i < index; i++)
-    {
-        /* update the previous node pointer */
-        prev = temp;
-
-        /* update the current node pointer */
-        temp = temp->next;
-    }
-
-    /* check if the node at index `index` was found */
-    if (temp == NULL)
-        return (-1);
-
-    /* update the previous node's next pointer */
-    prev->next = temp->next;
-
-    /* free the current node */
-    free(temp);
-
-    return (1);
+	/* check if the linked list is empty */
+	if (*head == NULL)
+		return (-1);
+	/* initialize the pointers */
+	temp = *head;
+	prev = NULL;
+	/* check if the index is 0 */
+	if (index == 0)
+	{
+		/* update the head pointer */
+		*head = temp->next;
+		/* free the current node */
+		free(temp);
+		return (1);
+	}
+	/* iterate through the linked list to find the node at index `index` */
+	for (i = 0; temp != NULL && i < index; i++)
+	{
+		/* update the previous node pointer */
+		prev = temp;
+		/* update the current node pointer */
+		temp = temp->next;
+	}
+	/* check if the node at index `index` was found */
+	if (temp == NULL)
+		return (-1);
+	/* update the previous node's next pointer */
+	prev->next = temp->next;
+	/* free the current node */
+	free(temp);
+	return (1);
 }
