c9d5e955f696f4a8d9cc0a5ece04e3f18a940f24
connortrue <connor.true.94@gmail.com>
2023-07-19 13:41:56 -0500

    bettyabbbillity

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0; /* number of nodes */
+	size_t count = 0; /* number of nodes */
 
-    while (h) /* iterate through the list */
-    {
-        printf("%d\n", h->n); /* print the node's data */
-        h = h->next; /* move to the next node */
-        count++; /* increment the node count */
-    }
+	while (h) /* iterate through the list */
+	{
+		printf("%d\n", h->n); /* print the node's data */
+		h = h->next; /* move to the next node */
+		count++; /* increment the node count */
+	}
 
-    return (count); /* return the number of nodes */
+	return (count); /* return the number of nodes */
 }
