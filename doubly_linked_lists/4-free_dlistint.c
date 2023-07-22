b8581ad66fa65c3f6e05abea3976266fda48b0ad
connortrue <connor.true.94@gmail.com>
2023-07-22 10:48:11 -0500

    moved declarations

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -5,13 +5,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	/* Check if the list is empty */
-	if (head == NULL)
-		return;
-
 	/* Declare two pointers to iterate through the list */
 	dlistint_t *current = head;
 	dlistint_t *next;
+
+	/* Check if the list is empty */
+        if (head == NULL)
+                return;
 
 	/* Iterate through the list, freeing each node in turn */
 	while (current != NULL)
