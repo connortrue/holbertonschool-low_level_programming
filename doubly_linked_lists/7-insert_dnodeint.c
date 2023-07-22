c4a03b6e7d22b8138941532889c5dc0af50fd73a
connortrue <connor.true.94@gmail.com>
2023-07-22 11:55:38 -0500

    forcing len again

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * dlistint_len - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0; /* number of nodes */
+
+	while (h) /* iterate through the list */
+	{
+		h = h->next; /* move to the next node */
+		count++; /* increment the node count */
+	}
+
+	return (count); /* return the number of nodes */
+}
+/**
  * insert_dnodeint_at_index - Inserts a new node at a given position in a
  *  dlistint_t linked list.
  * @h: The head of the list.
