296cf2ba4f5c3b52c0643210262ce0299340d1c4
connortrue <connor.true.94@gmail.com>
2023-07-22 17:00:17 -0500

    betty

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Set a value in a hash table.
+ *
+ * @ht: The hash table.
+ * @key: The key of the value to set.
+ * @value: The value to set.
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *temp;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
