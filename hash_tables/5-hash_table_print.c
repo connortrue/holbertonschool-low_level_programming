e9015b727ed37e32a23296dcf6eec6ecb2cb805c
connortrue <connor.true.94@gmail.com>
2023-07-22 17:28:01 -0500

    lets finally push

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,22 @@
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	char *comma = "";
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			printf("%s'%s': '%s'", comma, temp->key, temp->value);
+			comma = ", ";
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,23 @@
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp, *next;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
