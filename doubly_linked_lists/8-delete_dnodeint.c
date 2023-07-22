1361bcc2e59780eae2b66b6766ed7dbf954a6dd2
connortrue <connor.true.94@gmail.com>
2023-07-22 12:27:41 -0500

    is it not better

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,30 @@
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *prev;
+	unsigned int i;
+
+	if (head == NULL)
+		return (-1);
+
+	if (index == 0)
+		return (delete_dnodeint(head));
+
+	current = *head;
+	prev = NULL;
+	for (i = 0; current != NULL && i < index; i++)
+	{
+		prev = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	if (prev == NULL)
+		*head = current->next;
+	else
+		prev->next = current->next;
+
+	free(current);
+	return (1);
+}
