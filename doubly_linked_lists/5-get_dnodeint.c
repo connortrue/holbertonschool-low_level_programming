a443fca6406a4bdc13a731548168dc5958891e9c
connortrue <connor.true.94@gmail.com>
2023-07-22 11:30:36 -0500

    try to force inclusion of a counter

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * dlistint_len - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0; /* number of nodes */
+
+	while (h) /* iterate through the list */
+	{
+		h = h->next; /* move to the next node */
+		count++; /* increment the node count */
+	}
+
+	return (count); /* return the number of nodes */
+}
+
+/**
  * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
  * @head: The head of the list.
  * @index: The index of the node, starting from 0.
